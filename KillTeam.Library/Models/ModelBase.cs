e8568eff8de17078a11b9d21724882f68db93b40
John-Paul Smith <jps@johnpaulsmith.co.uk>
2022-03-22 18:03:03 +0000

    added library project, tidied up layout

---
+++ KillTeam.Library/Models/ModelBase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KillTeam.Library.Models
+{
+
+    /// <summary>Base class for all models</summary>
+    /// <remarks>Basically because im too lazy to include the same properties in lots of class. technically just code reuse!</remarks>
+    public class ModelBase
+    {
+        public Guid Id { get; set; }
+
+    }
+}
